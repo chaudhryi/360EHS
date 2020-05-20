716962c74c8efb6a358c45027c45f6153ed64a21
chaudhryi <ijazchaudhry@rogers.com>
2020-05-20 11:58:48 -0400

    deploy edits

--- EHS/settings.py
+++ EHS/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = ['ehs360.herokuapp.com','localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
