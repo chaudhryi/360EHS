fe84f1bba84bff45f0a7fbd14b0dc998c1ab9bfc
chaudhryi <ijazchaudhry@rogers.com>
2020-05-20 02:14:38 -0400

    deploy edits

--- EHS/settings.py
+++ EHS/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['ehs360.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['ehs360.herokuapp.com','localhost','127.0.0.1']
 
 
 # Application definition
