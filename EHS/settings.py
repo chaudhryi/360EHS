c456034edda2b9a242e88e40e977752dc36122de
chaudhryi <ijazchaudhry@rogers.com>
2020-05-21 22:19:01 -0400

    deploy edit

--- EHS/settings.py
+++ EHS/settings.py
@@ -118,11 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
+#     os.path.join(BASE_DIR, 'static'),
 # )
 
 # Simplified static file serving.
@@ -130,8 +132,8 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 LOGGING = {
     'version': 1,
