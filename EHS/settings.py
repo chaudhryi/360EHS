0229995b459ecbedc6dffdb756ecca0328d44ff8
chaudhryi <ijazchaudhry@rogers.com>
2020-05-23 15:53:12 -0400

    logging

--- EHS/settings.py
+++ EHS/settings.py
@@ -146,7 +146,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
         },
     },
 }
